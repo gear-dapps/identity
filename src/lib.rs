f7089566c6f0d88bd4680fce11a83f6f679843f5
Kirill Prosvirov <prosvirov.k@gmail.com>
2022-09-05 11:07:45 -0700

    Storage done (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,252 @@
 #![no_std]
 
-use gstd::{debug, msg, prelude::*};
+use gstd::{msg, prelude::*};
+use identity_io::*;
 
-#[no_mangle]
-unsafe extern "C" fn handle() {
-    debug!("handle()");
-    let payload = String::from_utf8(msg::load_bytes()).expect("Invalid handle message");
+#[derive(Debug, Default)]
+pub struct IdentityStorage {
+    user_claims: BTreeMap<PublicKey, BTreeMap<PieceId, Claim>>,
+    piece_counter: u128,
+}
 
-    if payload == "Hello" {
-        msg::reply(b"World", 0).unwrap();
+static mut IDENTITY: Option<IdentityStorage> = None;
+
+impl IdentityStorage {
+    /// Creates a new claim.
+    ///
+    /// # Requirements:
+    /// * all the public keys and signatures MUST be non-zero.
+    ///
+    /// # Arguments:
+    /// * `issuer` - the claim issuer's public key.
+    /// * `issuer_signature` - the corresponding signature with the `issuer` public key.
+    /// * `subject`- the subject's public key.
+    /// * `data` - claim's data.
+    fn issue_claim(
+        &mut self,
+        issuer: PublicKey,
+        issuer_signature: Signature,
+        subject: PublicKey,
+        data: ClaimData,
+    ) {
+        self.user_claims.entry(subject).or_default().insert(
+            self.piece_counter,
+            Claim {
+                issuer,
+                issuer_signature,
+                subject,
+                verifiers: BTreeMap::new(),
+                data,
+            },
+        );
+
+        msg::reply(
+            IdentityEvent::ClaimIssued {
+                issuer,
+                subject,
+                piece_id: self.piece_counter,
+            },
+            0,
+        )
+        .expect("IDENTITY: Error during replying with IdentityEvent::ClaimIssued");
+
+        self.piece_counter += 1;
+    }
+
+    /// Changes claim's validation status.
+    ///
+    /// # Requirements:
+    /// * all the public keys and signatures MUST be non-zero.
+    ///
+    /// # Arguments:
+    /// * `validator` - the claim issuer's or subject's public key.
+    /// * `subject`- the subject's public key.
+    /// * `piece_id` - claim's id.
+    /// * `status` - new claim's status.
+    fn change_validation_status(
+        &mut self,
+        validator: PublicKey,
+        subject: PublicKey,
+        piece_id: PieceId,
+        status: bool,
+    ) {
+        let data_piece = self
+            .user_claims
+            .get(&subject)
+            .expect("The user has no claims")
+            .get(&piece_id)
+            .expect("The user has not such claim with the provided piece_id");
+        if data_piece.subject != validator && data_piece.issuer != validator {
+            panic!("IDENTITY: You can not change this claim");
+        }
+        self.user_claims
+            .entry(subject)
+            .or_default()
+            .entry(piece_id)
+            .and_modify(|claim| claim.data.valid = status);
+
+        msg::reply(
+            IdentityEvent::ClaimValidationChanged {
+                validator,
+                subject,
+                piece_id,
+                status,
+            },
+            0,
+        )
+        .expect("IDENTITY: Error during replying with IdentityEvent::ClaimValidationChanged");
+    }
+
+    /// Verifies the claim.
+    ///
+    /// # Requirements:
+    /// * all the public keys and signatures MUST be non-zero.
+    /// * `verifier` - MUST differ from the claim's subject or issuer.
+    ///
+    /// # Arguments:
+    /// * `verifier` - the claim verifier's public key.
+    /// * `verifier_signature` - the corresponding signature with the `verifier` public key.
+    /// * `piece_id` - claim's id.
+    /// * `subject` - subject's public key.
+    fn verify_claim(
+        &mut self,
+        verifier: PublicKey,
+        verifier_signature: Signature,
+        subject: PublicKey,
+        piece_id: PieceId,
+    ) {
+        let piece = self
+            .user_claims
+            .get(&subject)
+            .expect("The user has no claims")
+            .get(&piece_id)
+            .expect("The user has not such claim with the provided piece_id");
+        if piece.issuer == verifier || piece.subject == verifier {
+            panic!("IDENTITY: You can not verify this claim");
+        }
+        self.user_claims
+            .entry(subject)
+            .or_default()
+            .entry(piece_id)
+            .and_modify(|claim| {
+                claim.verifiers.insert(verifier, verifier_signature);
+            });
+        msg::reply(
+            IdentityEvent::VerifiedClaim {
+                verifier,
+                subject,
+                piece_id,
+            },
+            0,
+        )
+        .expect("IDENTITY: Error during replying with IdentityEvent::VerifiedClaim");
     }
 }
 
 #[no_mangle]
-unsafe extern "C" fn init() {
-    let payload = String::from_utf8(msg::load_bytes()).expect("Invalid init message");
-    debug!("init(): {}", payload);
+extern "C" fn init() {
+    let id_storage = IdentityStorage {
+        piece_counter: 0,
+        ..Default::default()
+    };
+    unsafe {
+        IDENTITY = Some(id_storage);
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    extern crate std;
-
-    use gtest::{Log, Program, System};
-
-    #[test]
-    fn it_works() {
-        let system = System::new();
-        system.init_logger();
-
-        let program = Program::current(&system);
-
-        let res = program.send_bytes(42, "Let's start");
-        assert!(res.log().is_empty());
-
-        let res = program.send_bytes(42, "Hello");
-        let log = Log::builder().source(1).dest(42).payload_bytes("World");
-        assert!(res.contains(&log));
+#[gstd::async_main]
+async fn main() {
+    let action: IdentityAction = msg::load().expect("Unable to decode IdentityAction");
+    let identity = unsafe { IDENTITY.get_or_insert(Default::default()) };
+    match action {
+        IdentityAction::IssueClaim {
+            issuer,
+            issuer_signature,
+            subject,
+            data,
+        } => identity.issue_claim(issuer, issuer_signature, subject, data),
+        IdentityAction::ChangeClaimValidationStatus {
+            validator,
+            subject,
+            piece_id,
+            status,
+        } => identity.change_validation_status(validator, subject, piece_id, status),
+        IdentityAction::VerifyClaim {
+            verifier,
+            verifier_signature,
+            subject,
+            piece_id,
+        } => identity.verify_claim(verifier, verifier_signature, subject, piece_id),
     }
+}
+
+#[no_mangle]
+extern "C" fn meta_state() -> *mut [i32; 2] {
+    let state: IdentityStateQuery = msg::load().expect("Unable to decode IdentityStateQuery");
+    let identity = unsafe { IDENTITY.get_or_insert(Default::default()) };
+    let reply = match state {
+        IdentityStateQuery::UserClaims(pkey) => {
+            IdentityStateReply::UserClaims(match identity.user_claims.get(&pkey) {
+                None => BTreeMap::new(),
+                Some(claims) => claims.clone(),
+            })
+        }
+        IdentityStateQuery::Claim(pkey, piece_id) => IdentityStateReply::Claim(
+            identity
+                .user_claims
+                .entry(pkey)
+                .or_default()
+                .get(&piece_id)
+                .cloned(),
+        ),
+        IdentityStateQuery::ValidationStatus(pkey, piece_id) => {
+            let mut status = false;
+            if let Some(user_claim) = identity.user_claims.get(&pkey) {
+                if let Some(claim) = user_claim.get(&piece_id) {
+                    status = claim.data.valid
+                }
+            }
+            IdentityStateReply::ValidationStatus(status)
+        }
+        IdentityStateQuery::Date(pkey, piece_id) => {
+            let mut date: u64 = 0;
+            if let Some(user_claim) = identity.user_claims.get(&pkey) {
+                if let Some(claim) = user_claim.get(&piece_id) {
+                    date = claim.data.issuance_date
+                }
+            }
+            IdentityStateReply::Date(date)
+        }
+        IdentityStateQuery::Verifiers(pkey, piece_id) => {
+            let mut verifiers: Vec<PublicKey> = Vec::new();
+            if let Some(user_claim) = identity.user_claims.get(&pkey) {
+                if let Some(claim) = user_claim.get(&piece_id) {
+                    verifiers = claim.verifiers.keys().cloned().collect()
+                }
+            }
+            IdentityStateReply::Verifiers(verifiers)
+        }
+        IdentityStateQuery::CheckClaim(pkey, piece_id, hash) => {
+            let mut status = false;
+            if let Some(user_claim) = identity.user_claims.get(&pkey) {
+                if let Some(claim) = user_claim.get(&piece_id) {
+                    status = claim.data.hashed_info.contains(&hash)
+                }
+            }
+            IdentityStateReply::CheckedClaim(pkey, piece_id, status)
+        }
+    };
+    gstd::util::to_leak_ptr(reply.encode())
+}
+
+gstd::metadata! {
+    title: "Identity",
+    init:
+        input: InitIdentity,
+    handle:
+        input: IdentityAction,
+        output: IdentityEvent,
+    state:
+        input: IdentityStateQuery,
+        output: IdentityStateReply,
 }
---
+++ tests/utils.rs
@@ -0,0 +1,233 @@
+use gstd::prelude::*;
+use gtest::{Program, System};
+use identity_io::*;
+
+// MESSAGES
+pub fn init_identity(sys: &System, user: u64) -> Program {
+    sys.init_logger();
+    let id_program = Program::current(sys);
+    assert!(id_program.send(user, InitIdentity,).log().is_empty());
+
+    id_program
+}
+
+pub fn issue_claim_utils(
+    id_program: &Program,
+    user: u64,
+    claim: Claim,
+    piece_id: PieceId,
+    should_fail: bool,
+) {
+    let res = id_program.send(
+        user,
+        IdentityAction::IssueClaim {
+            issuer: claim.issuer,
+            issuer_signature: claim.issuer_signature,
+            subject: claim.subject,
+            data: claim.data,
+        },
+    );
+
+    if should_fail {
+        assert!(res.main_failed());
+    } else {
+        assert!(res.contains(&(
+            user,
+            IdentityEvent::ClaimIssued {
+                issuer: claim.issuer,
+                subject: claim.subject,
+                piece_id,
+            }
+            .encode()
+        )));
+    }
+}
+
+pub fn validation_claim_utils(
+    id_program: &Program,
+    user: u64,
+    validator: PublicKey,
+    subject: PublicKey,
+    piece_id: PieceId,
+    status: bool,
+    should_fail: bool,
+) {
+    let res = id_program.send(
+        user,
+        IdentityAction::ChangeClaimValidationStatus {
+            validator,
+            subject,
+            piece_id,
+            status,
+        },
+    );
+
+    if should_fail {
+        assert!(res.main_failed());
+    } else {
+        assert!(res.contains(&(
+            user,
+            IdentityEvent::ClaimValidationChanged {
+                validator,
+                subject,
+                piece_id,
+                status,
+            }
+            .encode()
+        )));
+    }
+}
+
+pub fn verify_claim_utils(
+    id_program: &Program,
+    user: u64,
+    verifier: PublicKey,
+    verifier_signature: Signature,
+    subject: PublicKey,
+    piece_id: PieceId,
+    should_fail: bool,
+) {
+    let res = id_program.send(
+        user,
+        IdentityAction::VerifyClaim {
+            verifier,
+            verifier_signature,
+            subject,
+            piece_id,
+        },
+    );
+
+    if should_fail {
+        assert!(res.main_failed());
+    } else {
+        assert!(res.contains(&(
+            user,
+            IdentityEvent::VerifiedClaim {
+                verifier,
+                subject,
+                piece_id,
+            }
+            .encode()
+        )));
+    }
+}
+
+// META-STATE
+pub fn check_claim_hash_state_utils(
+    id_program: &Program,
+    subject: PublicKey,
+    piece_id: PieceId,
+    hash: [u8; 32],
+    status: bool,
+) {
+    match id_program.meta_state(IdentityStateQuery::CheckClaim(subject, piece_id, hash)) {
+        Ok(IdentityStateReply::CheckedClaim(_, _, real_status)) => {
+            if real_status != status {
+                panic!("IDENTITY: Checking statuses differ")
+            }
+        }
+        _ => {
+            unreachable!(
+                "Unreachable metastate reply for the IdentityStateQuery::CheckClaim payload has occurred"
+            )
+        }
+    }
+}
+pub fn check_user_claims_state_utils(
+    id_program: &Program,
+    subject: PublicKey,
+    claims: BTreeMap<PieceId, Claim>,
+) {
+    match id_program.meta_state(IdentityStateQuery::UserClaims(subject)) {
+        Ok(IdentityStateReply::UserClaims(real_claims)) => {
+            if real_claims != claims {
+                panic!("IDENTITY: User claims differ")
+            }
+        }
+        _ => {
+            unreachable!(
+                "Unreachable metastate reply for the IdentityStateQuery::UserClaims payload has occurred"
+            )
+        }
+    }
+}
+
+pub fn check_claim_state_utils(
+    id_program: &Program,
+    subject: PublicKey,
+    piece_id: PieceId,
+    claim: Claim,
+) {
+    match id_program.meta_state(IdentityStateQuery::Claim(subject, piece_id)) {
+        Ok(IdentityStateReply::Claim(real_claim)) => {
+            if claim != real_claim.expect("IDENTITY: No such claim") {
+                panic!("IDENTITY: Claims differ");
+            }
+        }
+        _ => {
+            unreachable!(
+                "Unreachable metastate reply for the IdentityStateQuery::Claim payload has occurred"
+            )
+        }
+    }
+}
+
+pub fn check_verifiers_state_utils(
+    id_program: &Program,
+    subject: PublicKey,
+    piece_id: PieceId,
+    verifiers: Vec<PublicKey>,
+) {
+    match id_program.meta_state(IdentityStateQuery::Verifiers(subject, piece_id)) {
+        Ok(IdentityStateReply::Verifiers(real_verifiers)) => {
+            if real_verifiers != verifiers {
+                panic!("IDENTITY: Verifiers differ");
+            }
+        }
+        _ => {
+            unreachable!(
+                "Unreachable metastate reply for the IdentityStateQuery::Verifiers payload has occurred"
+            )
+        }
+    }
+}
+
+pub fn check_date_state_utils(
+    id_program: &Program,
+    subject: PublicKey,
+    piece_id: PieceId,
+    date: u64,
+) {
+    match id_program.meta_state(IdentityStateQuery::Date(subject, piece_id)) {
+        Ok(IdentityStateReply::Date(real_date)) => {
+            if real_date != date {
+                panic!("IDENTITY: Dates differ");
+            }
+        }
+        _ => {
+            unreachable!(
+                "Unreachable metastate reply for the IdentityStateQuery::Date payload has occurred"
+            )
+        }
+    }
+}
+
+pub fn check_valid_state_utils(
+    id_program: &Program,
+    subject: PublicKey,
+    piece_id: PieceId,
+    valid: bool,
+) {
+    match id_program.meta_state(IdentityStateQuery::ValidationStatus(subject, piece_id)) {
+        Ok(IdentityStateReply::ValidationStatus(real_valid)) => {
+            if real_valid != valid {
+                panic!("IDENTITY: Validation status differ");
+            }
+        }
+        _ => {
+            unreachable!(
+                "Unreachable metastate reply for the IdentityStateQuery::ValidationStatus payload has occurred"
+            )
+        }
+    }
+}
